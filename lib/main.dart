b58bccf85c0b8afdf91e05855795ecf76d1d5c81
radja <radjasatrio70@gmail.com>
2026-02-18 11:03:45 +0700

    Update main.dart buat inisialisasi audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fruit_game_catcher/managers/audio_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AudioManager().initialize();
   runApp(const MyApp());
 }
 
