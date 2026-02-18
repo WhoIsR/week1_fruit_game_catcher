cff3b1e26cf2adfb95af6c5b1ff84d5cbd58d886
radja <radjasatrio70@gmail.com>
2026-02-18 09:17:07 +0700

    setup audio manager singleton pattern

---
+++ lib/managers/audio_manager.dart
@@ -0,0 +1,12 @@
+import 'package:flame_audio/flame_audio.dart';
+
+class AudioManager {
+  static final AudioManager _instance = AudioManager._internal();
+  factory AudioManager() => _instance;
+  AudioManager._internal();
+
+  bool _isMusicEnabled = true;
+  bool _isSfxEnabled = true;
+  double _musicVolume = 0.7;
+  double _sfxVolume = 1.0;
+}
