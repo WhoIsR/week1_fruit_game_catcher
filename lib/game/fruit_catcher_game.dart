dda1316c211a892a1dfb52d91498ee96a65a36aa
radja <radjasatrio70@gmail.com>
2026-02-18 11:01:33 +0700

    Panggil BGM pas game loading

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -4,6 +4,11 @@ import '../managers/audio_manager.dart';
 
 class FruitCatcherGame extends FlameGame {
   @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    AudioManager().playBackgroundMusic();
+  }
+
   Color backgroundColor() => const Color(0xFF87CEEB);
 
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
